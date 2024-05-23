7e08801fde02741c2c732f6e07a471bd0d38dc37
Melina Iglesias <164907378+1melinaigl@users.noreply.github.com>
2024-05-23 14:34:21 -0300

    clase completa

--- src/AppConductor/AppConductor.java
+++ src/AppConductor/AppConductor.java
@@ -2,12 +2,70 @@ package AppConductor;
 
 import SEM.SEM;
 import Usuario.Usuario;
+import java.time.LocalTime;
+
+
 
 public class AppConductor {
-	
-	private SEM sem;
+
+    private SEM sem;
     private Usuario usuario;
     
+    
     public AppConductor(Usuario usuario) {
+    	
     }
+
+/* 
+    public void iniciarEstacionamiento() {
+        double saldoDeUsuario = this.sem.saldoDe(this.usuario.getCelular());
+        if(saldoDeUsuario >= 0) {
+            System.out.println("La hora de inicio es: " + LocalTime.now() + "La hora mÃ¡xima es:" + this.calcularHoraMaxima(saldoDeUsuario,LocalTime.now())); //hacer salto
+        EstacionamientoApp estacionamiento = new EstacionamientoApp(this.usuario, LocalTime.now());
+        this.sem.registrarEstacionamiento(estacionamiento);
+        } else {
+            System.out.println("no tenes saldo chabon");
+        }
+    }
+*/
+
+    public void iniciarEstacionamiento() {
+        if(elUsuarioTieneSaldoPositivo()) {
+            indicarHoraDeComienzoYHoraMaxima();
+            registrarEstacionamientoEnElSistema();
+        } else {
+            indicarSaldoInsuficiente();
+        }
+    }
+
+    private LocalTime calcularHoraMaxima(double saldoDeUsuario, LocalTime horaDeInicio) {
+     //calcular ;
+     return ;
+    }
+
+    public void finalizarEstacionamiento() {
+        EstacionamientoApp estacionamiento = new EstacionamientoApp(this.usuario, LocalTime.now());
+        this.sem.terminarEstacionamiento(estacionamiento);
+    }
+
+    public double consultarSaldo() {
+        return this.sem.saldoDe(this.usuario.getCelular());
+    }
+
+    public boolean elUsuarioTieneSaldoPositivo() {
+        return (consultarSaldo()>=0);
+    }
+
+    private void indicarHoraDeComienzoYHoraMaxima() {
+        ;
+    }
+    
+    private void registrarEstacionamientoEnElSistema() {
+        ;
+    }
+
+    private void indicarSaldoInsuficiente() {
+        ;
+    }
+
 }
