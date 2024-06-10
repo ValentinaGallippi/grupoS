7e0127129c8d093a15cd977f0a993db5d44fdcc5
roldanmarcos <guillermoroldan0@gmail.com>
2024-06-09 23:02:16 -0300

    test sem

--- test/grupoS/SEM/SEMTest.java
+++ test/grupoS/SEM/SEMTest.java
@@ -1,5 +1,33 @@
 package grupoS.SEM;
 
 public class SEMTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import grupoS.zonaDeEstacionamientoMedido.ZonaDeEstacionamientoMedido;
+
+class SEMTest {
+	
+	private SEM sem;
+	private ZonaDeEstacionamientoMedido zona;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		sem = new SEM(LocalTime.of(07, 00), LocalTime.of(20, 00), 40);
+		zona = mock(ZonaDeEstacionamientoMedido.class);
+	}
+
+	@Test
+	void cuandoSeRegistraUnaZonaDeEstacionamientoMedidoEnUnSEM_EstaPasaAFormarParteDeLasZonasDelSEM() {
+		sem.registrarZona(zona);
+		
+		assertTrue(sem.getZonas().contains(zona));
+	}
 
 }
